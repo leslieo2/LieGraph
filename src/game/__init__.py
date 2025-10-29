0ad9d70d2b020fd2cc65fbed339e83692f676f8c
leslie <ly624928214@gmail.com>
2025-10-29 19:35:16 +0800

    feat: add git workflow

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -4,9 +4,9 @@ Game module containing the core game logic.
 
 from . import nodes
 from . import state
-from . import llm_strategy
+from llm_strategy import llm_update_player_mindset, llm_update_player_mindset
 from . import rules
 from . import graph
 from . import config
 
-__all__ = ["nodes", "state", "llm_strategy", "rules", "graph", "config"]
+__all__ = ["nodes", "state", "llm_update_player_mindset", "llm_update_player_mindset", "rules", "graph", "config"]
